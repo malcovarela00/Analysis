15894da4897691f6e2b2148bd324f91607a198f2
malcovarela00 <117948203+malcovarela00@users.noreply.github.com>
2023-02-17 16:22:37 +0100

    fixed to upload

--- profit.py
+++ profit.py
@@ -6,7 +6,7 @@ def profit(df, date_start, date_end):
     df['date_end'] = pd.to_datetime(df['date_end'])
     mask = (df['date_end'] >= date_start) & (df['date_end'] <= date_end)
     df = df.loc[mask]
-    won = df.loc[df['status'] == 'won']                                     #estados won en 2022
+    won = df.loc[df['status'] == 'won']                                     #estados won
     won['Month'] = won['date_end'].dt.month                               #agrega una columna mes
     won['Year'] = won['date_end'].dt.year                                 #agrega una colmna año
     
@@ -23,15 +23,6 @@ def profit(df, date_start, date_end):
     for p in ax.patches:
         ax.annotate(format(p.get_height(), '.0f'), (p.get_x() + p.get_width() / 2., p.get_height()), ha = 'center', va = 'center', xytext = (0, 10), textcoords = 'offset points')
 
-    plt.savefig('gv/graficos/profit.png')
+    plt.savefig('./graficos/profit.png')
     plt.show()
     plt.close()
-
-df = pd.read_csv('./gv/data/bi_deal(2021a).csv')
-
-## La fecha límite de inicio es 2019-01-01
-
-date_start = '2022-01-01'
-date_end = '2023-01-31'
-
-profit(df, date_start, date_end)
