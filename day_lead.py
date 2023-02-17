edb29f95d7acaf31c66f4c74153fe1e53d51c385
malcovarela00 <117948203+malcovarela00@users.noreply.github.com>
2023-02-17 16:24:52 +0100

    fixed to upload

--- day_lead.py
+++ day_lead.py
@@ -7,6 +7,7 @@ def DayLead(df, date_start, date_end):
     df['date_end'] = pd.to_datetime(df['date_end'])
     mask = (df['date_end'] >= date_start) & (df['date_end'] <= date_end)
     df = df.loc[mask]
+    
     df['dia'] = df['date_end'].dt.dayofweek
     dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
     
@@ -19,13 +20,3 @@ def DayLead(df, date_start, date_end):
     plt.ylabel('Cantidad de leads')
     plt.savefig('gv/graficos/day_lead.png')
     plt.show()
-
-
-df = pd.read_csv('./gv/data/bi_deal(2021a).csv')
-
-## La fecha límite de inicio es 2019-01-01
-
-date_start = '2022-01-01'
-date_end = '2023-01-31'
-
-DayLead(df, date_start, date_end)
