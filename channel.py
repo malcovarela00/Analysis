ebd249397a21ba3538173ec2be8772bed842e9f0
malcovarela00 <117948203+malcovarela00@users.noreply.github.com>
2023-02-17 16:24:11 +0100

    fixed to upload

--- channel.py
+++ channel.py
@@ -6,8 +6,8 @@ def channel(df, date_start, date_end):
     df['date_end'] = pd.to_datetime(df['date_end'])
     mask = (df['date_end'] >= date_start) & (df['date_end'] <= date_end)
     df = df.loc[mask]
+    
     result = pd.DataFrame(df["channel"].value_counts())
-    print(result)
     result.plot(kind='pie', subplots=True, figsize=(25, 15))
     plt.title('Lead por canal' + ' del ' + str(date_start) + ' a ' + str(date_end))
     plt.savefig('./graficos/channel_lead.png')
